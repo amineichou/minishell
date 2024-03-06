b5c2bfd5e62b5ef2c93507fc956a5491f8c9eae7
amineichou <ichouamine2020@gmail.com>
2024-03-06 14:55:30 +0100

    first commit

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: moichou <moichou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 13:17:11 by moichou           #+#    #+#             */
-/*   Updated: 2024/03/06 14:35:36 by moichou          ###   ########.fr       */
+/*   Updated: 2024/03/06 14:53:55 by moichou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,23 @@
 #include <stdio.h>
 #include <readline/readline.h>
 
+
+typedef struct s_env {
+	char			*var;
+	struct s_env	*next;
+} t_env;
+
 // parsing
 typedef struct s_token {
-	
+	int				input;
+	int				output;
+	char			**args;
+	t_env 			*env;
+	struct s_token	*next;
+	struct s_token	*prev;
 }	t_token;
+
+
 
 
 t_token	ft_parsing(char *line);
