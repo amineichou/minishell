5d870ab1748bf400eed77fc23664973a42c76b64
amineichou <ichouamine2020@gmail.com>
2024-03-06 14:38:00 +0100

    first commit

--- minishell.h
+++ minishell.h
@@ -6,13 +6,22 @@
 /*   By: moichou <moichou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 13:17:11 by moichou           #+#    #+#             */
-/*   Updated: 2024/03/06 13:19:08 by moichou          ###   ########.fr       */
+/*   Updated: 2024/03/06 14:35:36 by moichou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
+#include <stdio.h>
+#include <readline/readline.h>
 
+// parsing
+typedef struct s_token {
+	
+}	t_token;
+
+
+t_token	ft_parsing(char *line);
 
 #endif
