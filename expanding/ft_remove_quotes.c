aff52caf782279058616b9b83097726082bfbe5b
amineichou <ichouamine2020@gmail.com>
2024-04-18 22:42:46 +0100

    fix expanding

--- expanding/ft_remove_quotes.c
+++ expanding/ft_remove_quotes.c
@@ -6,13 +6,28 @@
 /*   By: moichou <moichou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 01:40:17 by moichou           #+#    #+#             */
-/*   Updated: 2024/04/18 19:18:08 by moichou          ###   ########.fr       */
+/*   Updated: 2024/04/18 21:37:31 by moichou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static char	*ft_get_inside_qoutes(char *str, int *i, int type)
+{
+	char	*res;
+	int		start;
+	int		length;
 
+	start = *i;
+	length = 0;
+	while (str[*i] && ft_isquote(str[*i]) != type)
+	{
+		(*i)++;
+		length++;
+	}
+	res = ft_strldup(&str[start], length);
+	return (res);
+}
 
 char	*ft_remove_qoutes(char *str)
 {
@@ -26,9 +41,16 @@ char	*ft_remove_qoutes(char *str)
 	while (str[i])
 	{
 		start = i;
-		if (ft_isquote(str[i + 1]))
-			inside_quotes = ft_get_inside_qoutes(str, &i);
-		
+		if (str[i] && ft_isquote(str[i]))
+			inside_quotes = ft_get_inside_qoutes(str, &i, ft_isquote(str[i]));
+		printf("%s\n", inside_quotes);
 		i++;
-	}	
+	}
+	return (res);
+}
+
+int main(void)
+{
+	char *test = ft_strdup("amine 'ichou' hello world");
+	char *res = ft_remove_qoutes(test);
 }
--- expanding/test.sh
+++ expanding/test.sh
@@ -1,1 +1,1 @@
-gcc -g ft_expand_test.c ../parsing/libft.c ../parsing/tools/*.c ../parsing/ft_split.c ../free_leaks/*.c -fsanitize=address  && ./a.out
+gcc -g ft_remove_quotes.c ../parsing/libft.c ../parsing/tools/*.c ../parsing/ft_split.c ../free_leaks/*.c -fsanitize=address  && ./a.out
