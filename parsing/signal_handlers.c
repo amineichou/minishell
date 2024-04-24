a9c1e7952c6a4e3c31dddd0a0c1a1109e57f0cf4
amineichou <ichouamine2020@gmail.com>
2024-04-24 10:25:15 +0100

    signal fixed

--- parsing/signal_handlers.c
+++ parsing/signal_handlers.c
@@ -6,7 +6,7 @@
 /*   By: moichou <moichou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 23:33:14 by moichou           #+#    #+#             */
-/*   Updated: 2024/04/23 16:56:53 by moichou          ###   ########.fr       */
+/*   Updated: 2024/04/24 10:23:05 by moichou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 void	ft_sigkill_handler(int signum)
 {
-	(void)signum;
-	rl_on_new_line();
-	printf("\n");
-	rl_replace_line("", 1);
-	rl_redisplay();
+	if (signum == 2)
+	{
+		rl_on_new_line();
+		printf("\n");
+		rl_replace_line("", 1);
+		rl_redisplay();	
+	}
 }
